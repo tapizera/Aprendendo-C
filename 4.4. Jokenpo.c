020635e0aee2e3fab01c36e49c95d0c44baa6823
tapizera <d4vifalcao2008@gmail.com>
2026-02-21 17:27:40 -0300

    4.4. Jokenpo Finalizado

--- 4.4. Jokenpo.c
+++ 4.4. Jokenpo.c
@@ -1,84 +1,63 @@
-// data: 20/02/2026, sex, 18:20
+// data: 21/02/2026, sáb, 17:00 (com ajuda do GPT)
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    printf("\n--- Pedra, Papel, Tesoura ---\n");
-    printf("1 - Jogar\n");
-    printf("2 - Sair\n");
-    printf("O que deseja?: ");
-    int menu;
-    scanf("%d", &menu);
-    
-    switch (menu) {
-        // jogar
-        case 1:
-            printf("\n--- Jogar ---\n");
-            printf("1 - Pedra\n");
-            printf("2 - Papel\n");
-            printf("3 - Tesoura\n");
-            printf("O que deseja?: ");
-            int player;
-            scanf("%d", &player);
-
-            // numero aleatorio do pc, precisa da biblioteca <stdlib.h> importada acima
-            int pc;
-            scanf("%d", rand() % 3);
-            
-            // pontuação
-            int pontosdoplayer = 0;
-            int pontosdopc = 0;
-            // se ganhar
-            // pontos += 1;
-
-
-            // rock > tesoura
-            if (player == 1 && pc == 3) {
-                pontosdoplayer += 1;
-            }
-            // rock < papel
-            else if (player == 1 && pc == 2) {
-                pontosdopc += 1;
-            }
-            // rock = rock
-            else if (player == 1 && pc == 1) {
-                printf("empate");
-            }
-
-
-            // papel < tesoura
-            else if (player == 2 && pc == 3) {
-                pontosdopc += 1;
-            }
-            // papel = papel
-            else if (player == 2 && pc == 2) {
-                printf("empate");
-            }
-            // papel > rock
-            else if (player == 2 && pc == 1) {
-                pontosdoplayer += 1;
-            }
-
-
-            // tesoura < rock
-            else if (player == 3 && pc == 1) {
-                pontosdopc += 1;
-            }
-            // tesoura > papel
-            else if (player == 3 && pc == 2) {
-                pontosdoplayer += 1;
-            }
-            // tesoura = tesoura
-            else if (player == 3 && pc == 3) {
-                printf("empate");
-            }
-
-        // sair do jogo
-        case 2:
-            printf("Bye bye...");
-            break;
+char* emoji(int escolha) {
+    switch(escolha) {
+        case 1: return "👊";
+        case 2: return "✋";
+        case 3: return "✌️";
+        default: return "?";
     }
+}
+
+void jokenpo(int *pontosdoplayer, int *pontosdopc) {
+
+    if (*pontosdoplayer == 3 || *pontosdopc == 3) {
+        printf("\n--- FIM DE JOGO ---\n");
+        printf("Player  %d X %d  PC\n\n", *pontosdoplayer, *pontosdopc);
+        return;
+    }
+    else {
+        printf("\n--- Jokenpo ---");
+    }
+
+    printf("\n1 - Pedra 👊\n2 - Papel✋\n3 - Tesoura✌️\nEu escolho: ");
+
+    int player;
+    scanf("%d", &player);
+
+    int pc = (rand() % 3) + 1;
+
+    // printf("PC escolheu: %d\n", pc);
+    // vou colocar a escolha na tabelinha de Player X PC
+
+    if (player == pc) {
+        printf("\nEmpate!");
+        printf("\nPlayer %s X %s PC", emoji(player), emoji(pc));
+    }
+    else if ((player == 1 && pc == 3) || (player == 2 && pc == 1) || (player == 3 && pc == 2)){
+        printf("\nPlayer %s X %s PC", emoji(player), emoji(pc));
+        (*pontosdoplayer)++;
+    }
+    else {
+        printf("\nPlayer %s X %s PC", emoji(player), emoji(pc));
+        (*pontosdopc)++;
+    }
+
+    printf("\nPlayer  %d X %d  PC\n", *pontosdoplayer, *pontosdopc);
+
+    jokenpo(pontosdoplayer, pontosdopc); // recursão controlada
+}
+
+int main() {
+    int pontosdoplayer = 0;
+    int pontosdopc = 0;
+
+    srand(time(NULL));
+
+    jokenpo(&pontosdoplayer, &pontosdopc);
 
     return 0;
 }
